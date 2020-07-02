8ea05d829883198c8e2c22198740c4467d54f66a
EngBett <bettthetimetraveller@gmail.com>
2020-07-02 15:25:00 +0300

    G service

--- GmailRestApi/Program.cs
+++ GmailRestApi/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
 
 namespace GmailRestApi
 {
@@ -7,9 +10,23 @@ namespace GmailRestApi
         static void Main(string[] args)
         {
             
+            /*var gmailService = new GService().Service();
+            var gmailRepo = new Repository();
+            List<Message> messages = gmailRepo.ListMessages(gmailService, "me", "");
+
+            foreach (var message in messages)
+            {
+                
+            }*/
             
+            print("hello");
             
             Console.Read();
+        }
+
+        static void print(Object obj)
+        {
+            Console.WriteLine(obj);
         }
     }
 }
